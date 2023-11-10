b5a5373edcbd2bac8db5326681b96acd0ae84df4
derodero24 <k.itsh20524abc@gmail.com>
2023-11-11 03:18:22 +0900

    存在しないテクスチャファイルはスキップ

--- viewer/lib/utils.js
+++ viewer/lib/utils.js
@@ -1,28 +1,38 @@
-function safeRequire (path) {
+const fs = require('fs');
+
+function safeRequire(path) {
   try {
-    return require(path)
+    return require(path);
   } catch (e) {
-    return {}
+    return {};
   }
 }
-const { loadImage } = safeRequire('node-canvas-webgl/lib')
-const THREE = require('three')
-const path = require('path')
+const { loadImage } = safeRequire('node-canvas-webgl/lib');
+const THREE = require('three');
+const path = require('path');
 
-const textureCache = {}
-function loadTexture (texture, cb) {
+const textureCache = {};
+function loadTexture(texture, cb) {
   if (textureCache[texture]) {
-    cb(textureCache[texture])
+    cb(textureCache[texture]);
   } else {
-    loadImage(path.resolve(__dirname, '../../public/' + texture)).then(image => {
-      textureCache[texture] = new THREE.CanvasTexture(image)
-      cb(textureCache[texture])
-    })
+    const texturePath = path.resolve(__dirname, '../../public/' + texture);
+
+    // ファイルが存在しない場合はスキップ
+    if (!fs.existsSync(texturePath)) {
+      console.warn('[WARN] texture not found', texturePath);
+      return;
+    }
+
+    loadImage(texturePath).then(image => {
+      textureCache[texture] = new THREE.CanvasTexture(image);
+      cb(textureCache[texture]);
+    });
   }
 }
 
-function loadJSON (json, cb) {
-  cb(require(path.resolve(__dirname, '../../public/' + json)))
+function loadJSON(json, cb) {
+  cb(require(path.resolve(__dirname, '../../public/' + json)));
 }
 
-module.exports = { loadTexture, loadJSON }
+module.exports = { loadTexture, loadJSON };
